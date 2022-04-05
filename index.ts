06d05dcd830cb51a498f65a52a554ec6da61bb5f
Camilo Ortiz <camilo_ortiz.203@hotmail.com>
2022-04-05 16:40:10 -0500

    Completado

---
+++ index.ts
@@ -0,0 +1,26 @@
+import {Serie} from "./serie.js"
+import {series} from "./data.js"
+
+const coursesTbody: HTMLElement = document.getElementById('series')!;
+renderCoursesInTable(series)
+
+function renderCoursesInTable(series: Serie[]): void {
+    let suma: number = 0;
+    let total: number = 0;
+    series.forEach(c => {
+        let trElement = document.createElement("tr");
+        trElement.innerHTML = `<td>${c.id}</td>
+        <td>${c.name}</td>
+        <td>${c.channel}</td>
+        <td>${c.seasons}</td>`;
+        coursesTbody.appendChild(trElement);
+    });
+    series.forEach(c => {
+        suma += c.seasons;
+        total += 1;
+    })
+    let trElement = document.createElement("tr");
+    trElement.innerHTML= `<td colspan="4">Seasons average: ${suma/total}</td>`;
+    coursesTbody.appendChild(trElement);
+
+}
